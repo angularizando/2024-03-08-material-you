22477536397ed53104d4cf983b0d80ba962337d2
Rafael Paiva <r.paivabr@gmail.com>
2024-08-11 11:24:10 -0300

    final example without experimental - v18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'material-you';
+  title = 'Material You Demo';
+  count = signal(0);
+
+  increment(): void {
+    this.count.update(value => value + 1);
+  }
 }
